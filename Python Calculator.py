322426d254e38219edd79b6cfbd37d81e2eaf875
Ole Kristoffer <Okritran@outlook.com>
2021-08-24 13:17:21 +0200

    Regner ut areal og omkrets av firkant i python

---
+++ Python Calculator.py
@@ -0,0 +1,8 @@
+høyde = int(input("Høyden: "))
+bredde = int(input("Bredden: "))
+
+areal = høyde * bredde
+omkrets = høyde * 2 + bredde * 2
+
+print("\nArealet er: " + str(areal))
+print("Omkretsen er: " + str(omkrets))
