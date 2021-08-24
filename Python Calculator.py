cdeba3cd000be425aaeec751417f316f2a86987b
Ole Kristoffer <Okritran@outlook.com>
2021-08-24 13:29:30 +0200

    La til areal av trekant

--- Python Calculator.py
+++ Python Calculator.py
@@ -1,8 +1,37 @@
-høyde = int(input("Høyden: "))
-bredde = int(input("Bredden: "))
+from time import sleep
 
-areal = høyde * bredde
-omkrets = høyde * 2 + bredde * 2
+on = True
 
-print("\nArealet er: " + str(areal))
-print("Omkretsen er: " + str(omkrets))
+while on:
+        
+    print("\nHva vil du regne ut?:")
+    print("\n   1 - Areal og Omkrets av firkant")
+    print("   2 - Areal av trekant")
+    print("\n   0 - Avslutt")
+
+    valg = int(input("\nSvar: "))
+
+    if valg == 1:
+
+        høyde = int(input("\nHøyden: "))
+        bredde = int(input("Bredden: "))
+
+        areal = høyde * bredde
+        omkrets = høyde * 2 + bredde * 2
+
+        print("\nArealet er: " + str(areal))
+        print("Omkretsen er: " + str(omkrets))
+
+    elif valg == 2:
+            
+        grunnlinje = int(input("\nGrunnlinjen: "))
+        høyde = int(input("Høyden: "))
+
+        areal = (grunnlinje * høyde) / 2
+
+        print("Arealet av trekanten er: " + str(areal))
+
+    else:
+        print("\nVelg et av alternativene over!")
+    
+    sleep(2)
